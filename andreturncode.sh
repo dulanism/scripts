bec34854dba0c40c7bc08ef245574927d98f790d
dulanism <dulani@dulani.com>
2016-04-09 15:16:04 -0400

    exit stat script added

---
+++ andreturncode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HOST="google.com"
+pimg -c 1 $HOST && echo "$HOST reachable."
+
+#And statement used;if ping command exits with a 0 exit status then google.com  reachable will be echoed to the screen
+
